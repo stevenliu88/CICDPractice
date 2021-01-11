eccfef8f59f69cb26cbcc2d43418a6be207f6d92
stevenliu88 <30949641+stevenliu88@users.noreply.github.com>
2021-01-11 13:13:37 +1100

    update test cases

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "steven";
+            return "steven learning CI/CD";
         }
 
         // POST api/values
--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace SimpleAPI.Test
         public void TestValue()
         {
             var result = controller.Get(2);
-            Assert.Equal("steven",result.Value);
+            Assert.Equal("steven learning CI/CD",result.Value);
         }
         [Fact]
         public void Test1()
