4519205cce885c47d53e82c839f730813daa381f
stevenliu88 <30949641+stevenliu88@users.noreply.github.com>
2021-01-11 12:57:42 +1100

    Update test cases

--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace SimpleAPI.Test
         public void TestValue()
         {
             var result = controller.Get(2);
-            Assert.Equal("test",result.Value);
+            Assert.Equal("steven",result.Value);
         }
         [Fact]
         public void Test1()
